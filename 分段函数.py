42fa25f228259627e1d1164f7e96473d601bdcb1
unknown <2028167903@qq.com>
2020-07-16 20:53:33 +0800

    换硬币

---
+++ 分段函数.py
@@ -0,0 +1,9 @@
+x=float(input())
+if(x==3):
+    result=10.00
+    print("f(3.00)=result")
+if(x<3):
+    print("f({:.2f})=1.20".format(x))
+else:
+    print("f({:.2f})={:.2f}".format(x，2*x+1))
+    
